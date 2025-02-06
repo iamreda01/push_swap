17d620d3801eb857cc6d4e8394d0afae030e72f7
Rida El kassari <rel-kass@e2r5p6.1337.ma>
2025-02-06 05:04:05 +0100

    done

--- moves/moves.c
+++ moves/moves.c
@@ -1,16 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push.c                                             :+:      :+:    :+:   */
+/*   moves.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rel-kass <rel-kass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/06 00:38:25 by rel-kass          #+#    #+#             */
-/*   Updated: 2025/02/06 03:20:35 by rel-kass         ###   ########.fr       */
+/*   Created: 2025/02/06 04:48:13 by rel-kass          #+#    #+#             */
+/*   Updated: 2025/02/06 04:48:35 by rel-kass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+void	swap(t_list *list)
+{
+	if (ft_lstsize(list) <= 1)
+		return ;
+	swap_int(list->content, list->next->content);
+}
 
 void	push(t_list **from, t_list **to)
 {
@@ -22,4 +29,18 @@ void	push(t_list **from, t_list **to)
 	(*from)->next = *to;
 	(*to) = (*from);
 	*from = tmp;
+}
+
+void	rotate(t_list **lst)
+{
+	if (!lst || !*lst)
+		return ;
+	t_list *tmp;
+	t_list *last;
+	
+	tmp = (*lst)->next;
+	last = ft_lstlast(*lst);
+	(*lst)->next = NULL;
+	last->next = *lst;
+	lst = tmp;
 }
